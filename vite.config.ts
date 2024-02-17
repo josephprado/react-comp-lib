c2a3be33d5efd96e32bda3f8df6a8cd799bc1d1d
prado <pradodje@gmail.com>
2024-02-17 04:00:10 -0800

    No CSS injection

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 import dts from 'vite-plugin-dts';
-import { extname, relative, resolve } from 'path';
-import { fileURLToPath } from 'node:url';
-import { glob } from 'glob';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    libInjectCss(),
     dts({
       include: ['lib/**/!(*.spec|*.test).{ts,tsx}'],
       exclude: ['lib/test/**'],
@@ -30,22 +26,6 @@ export default defineConfig({
     copyPublicDir: false,
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
-      input: Object.fromEntries(
-        glob
-          .sync('lib/**/!(*.spec|*.test).{ts,tsx}', { ignore: 'lib/test/**' })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative('lib', file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ]),
-      ),
-      output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
-      },
     },
   },
   test: {
