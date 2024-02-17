694d3c20876d53688b5293947b9428f36a65534c
prado <pradodje@gmail.com>
2024-02-17 01:19:10 -0800

    Add animated expanding demo

--- src/expanding-context-demo/expanding-context-demo.tsx
+++ src/expanding-context-demo/expanding-context-demo.tsx
@@ -4,6 +4,7 @@ import { withExpandingContext } from '../../lib/components/expanding-context/wit
 import { DemoSection } from '../components/demo-section';
 import { ExampleContainer } from '../components/example-container';
 import styles from './expanding-context-demo.module.scss';
+import clsx from 'clsx';
 
 function ExpandingComponent() {
   const { expanded, setExpanded } = useExpandingContext();
@@ -17,7 +18,10 @@ function ExpandingComponent() {
         Click me to {expanded ? 'collapse' : 'expanded'}!
       </div>
       {expanded && (
-        <div className={styles.sometimesVisible}>
+        <div
+          className={styles.sometimesVisible}
+          onClick={(event) => event.stopPropagation()}
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -28,11 +32,42 @@ function ExpandingComponent() {
   );
 }
 
+function AnimatedExpandingComponent() {
+  const { expanded, setExpanded } = useExpandingContext();
+
+  return (
+    <div
+      className={clsx(
+        styles.expandingContainer,
+        styles.animated,
+        !expanded && styles.collapsed,
+      )}
+      onClick={() => setExpanded((prev) => !prev)}
+    >
+      <div className={styles.alwaysVisible}>
+        Click me to {expanded ? 'collapse' : 'expanded'}!
+      </div>
+      <div
+        className={clsx(styles.sometimesVisible, !expanded && styles.collapsed)}
+        onClick={(event) => event.stopPropagation()}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </div>
+    </div>
+  );
+}
+
 export function ExpandingContextDemo() {
   const [reset, setReset] = useState<boolean>(false);
 
   const DemoComponentContracted = withExpandingContext(ExpandingComponent);
   const DemoComponentExpanded = withExpandingContext(ExpandingComponent, true);
+  const DemoAnimatedExpandingComponent = withExpandingContext(
+    AnimatedExpandingComponent,
+  );
 
   useEffect(() => {
     if (reset) setReset(false);
@@ -47,6 +82,10 @@ export function ExpandingContextDemo() {
 
         <ExampleContainer title="Default Expanded">
           <DemoComponentExpanded />
+        </ExampleContainer>
+
+        <ExampleContainer title="Animated Expansion">
+          <DemoAnimatedExpandingComponent />
         </ExampleContainer>
       </div>
       <button type="button" onClick={() => setReset(true)}>
